173db120682037a696d800d7dd0688414322e119
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2024-03-31 18:01:17 +0800

    solved 283. Move Zeroes

--- 283_movezeroes.py
+++ 283_movezeroes.py
@@ -11,4 +11,13 @@
 #     print(non_zero+zeroes)
 
 def moveZeroes(nums):
-    
+    left_ptr = 0
+    right_ptr = 0
+
+    for _ in nums:
+        if nums[right_ptr] != 0:
+            nums[left_ptr], nums[right_ptr] = nums[right_ptr], nums[left_ptr]
+            left_ptr += 1
+            right_ptr += 1
+        else:
+            right_ptr += 1
