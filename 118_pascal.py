4ffc7a0e471ddd3ad1867115cd878043eab221b3
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2024-03-31 20:30:16 +0800

    solved 118. Pascal's Triangle

---
+++ 118_pascal.py
@@ -0,0 +1,11 @@
+def generate(numRows):
+    res = []
+
+    for i in range(numRows):
+        res.append([])
+        for j in range(0, i+1):
+            if j == 0 or j == i:
+                res[i].append(1)
+            else:
+                res[i].append(res[i-1][j-1] + res[i-1][j])
+    return res
