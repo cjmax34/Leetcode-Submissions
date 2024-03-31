0098123aa46282ed52dcf7f23fcbd6737fc1c247
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2024-03-31 21:31:47 +0800

    solved 46. Permutations

--- 46_permutations.py
+++ 46_permutations.py
@@ -1,2 +1,15 @@
 def permute(nums):
-    if len(nums)
+    if len(nums) == 0:
+        return []
+
+    if len(nums) == 1:
+        return [nums]
+
+    all_perms = []
+
+    for i in range(len(nums)):
+        new_lst = nums[:i] + nums[i+1:]
+        for j in permute(new_lst):
+            all_perms.append([nums[i]]+j)
+        
+    return all_perms
--- 867_transposematrix.py
+++ 867_transposematrix.py
@@ -1,4 +1,2 @@
 def transpose(matrix):
     return list(map(list, zip(*matrix)))
-
-print(transpose([[1,2,3],[4,5,6],[7,8,9]]))
