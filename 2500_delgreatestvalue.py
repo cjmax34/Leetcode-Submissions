a22091bfddebc6799304996eeafb74cd5737cec1
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2024-04-01 16:55:21 +0800

    solved 2500. Delete Greatest Value in Each Row

--- 2500_delgreatestvalue.py
+++ 2500_delgreatestvalue.py
@@ -1,17 +1,10 @@
 def deleteGreatestValue(grid):
     sum = 0
-    m = grid.length     # rows length
-    n = grid[0].length  # cols length
+    
+    for nums in grid:
+        nums.sort(reverse=True)
 
-    while m and n:
-        max_1 = max(grid[0])
-        max_2 = max(grid[1])
-
-        grid[0].remove(max_1)
-        grid[1].remove(max_2)
-
-        sum += max(max_1, max_2)
+    for nums in list(zip(*grid)):
+        sum += max(nums)
 
     return sum
-
-print(deleteGreatestValue([[10]]))
