42eebaa6b1bcb573f476cf0bc1dd1c1dac86e4a8
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2024-04-01 13:35:15 +0800

    solved 349. Intersection of Two Arrays

--- 2500_delgreatestvalue.py
+++ 2500_delgreatestvalue.py
@@ -1,2 +1,17 @@
 def deleteGreatestValue(grid):
     sum = 0
+    m = grid.length     # rows length
+    n = grid[0].length  # cols length
+
+    while m and n:
+        max_1 = max(grid[0])
+        max_2 = max(grid[1])
+
+        grid[0].remove(max_1)
+        grid[1].remove(max_2)
+
+        sum += max(max_1, max_2)
+
+    return sum
+
+print(deleteGreatestValue([[10]]))
