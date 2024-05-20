e0efbe76390d8fc3c1752e8b0d6053b9f09b27ed
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-05-20 14:08:00 +0800

    solved 2828. Check if a String Is an Acronym of Words

---
+++ 2828_acronym.py
@@ -0,0 +1,11 @@
+def isAcronym(words, s):
+    if len(words) != len(s):
+        return False
+
+    for i in range(len(words)):
+        if words[i][0] != s[i]:
+            return False
+        
+    return True
+
+print(isAcronym(["never","gonna","give","up","on","you"], "ngguoy"))
