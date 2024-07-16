a9ddda73d2b70c75b7376a33ef2975b502567c57
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2024-07-16 17:25:28 +0800

    chore: add gitignore for workspace

---
+++ 169.majority-element.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        curr = nums[0]
+        count = 0
+        for num in nums:
+        
+# @lc code=end
+
