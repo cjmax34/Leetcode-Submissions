3685f3a488fd23ddf7dd8a3914cc5dc72ab1c7f6
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-02-12 20:55:01 +0800

    solved 15. 3Sum

---
+++ 1-100/15.3sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        n = len(nums)
+        nums.sort()
+
+        for i in range(n-1):
+            x = nums[i] * -1
+            j, k = i+1, n-1
+
+            while j < k:
+                if nums[j] + nums[k] > x:
+                    k -= 1
+                elif nums[j] + nums[k] < x:
+                    j += 1
+                else: 
+                    res.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+
+        return list(res)
