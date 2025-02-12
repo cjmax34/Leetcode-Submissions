07a75e0448c46dce8350208b8256af2665f879df
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-02-12 22:17:57 +0800

    two pointer solution to 42. Trapping Rain Water

--- 101-200/42.trapping-rain-water.py
+++ 101-200/42.trapping-rain-water.py
@@ -1,21 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        leftMax = [0] * n
-        rightMax = [0] * n
+        leftMax, rightMax = height[0], height[-1]
+        left, right = 0, len(height)-1
         sum = 0
 
-        leftMax[0], rightMax[-1] = height[0], height[-1]
-
-        for i in range(n):
-            leftMax[i] = max(leftMax[i-1], height[i])
-
-        for i in range(n-2, -1, -1):
-            rightMax[i] = max(rightMax[i+1], height[i])
-
-        print(rightMax)
-
-        for i in range(n):
-            sum += min(leftMax[i], rightMax[i]) - height[i]
+        while left < right:
+            if height[left] < height[right]:
+                left += 1
+                leftMax = max(leftMax, height[left])
+                sum += max(0, leftMax-height[left])
+            else:
+                right -= 1
+                rightMax = max(rightMax, height[right])
+                sum += max(0, rightMax-height[right])
 
         return sum
