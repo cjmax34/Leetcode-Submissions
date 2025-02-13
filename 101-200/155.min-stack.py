e0073ab8c3c016e939c457d985b91e81e5225c14
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-02-13 14:10:00 +0800

    solved 155. Min Stack

--- 101-200/155.min-stack.py
+++ 101-200/155.min-stack.py
@@ -1,21 +1,24 @@
 class MinStack:
 
     def __init__(self):
+        self.stk = []
+        self.currMinList = []
         
-
     def push(self, val: int) -> None:
-        
+        self.stk.append(val)
+        currMin = val if not self.currMinList else min(self.currMinList[-1][1], val)
+        self.currMinList.append((val, currMin))
 
     def pop(self) -> None:
+        self.stk.pop()
+        self.currMinList.pop()
         
-
     def top(self) -> int:
+        return self.stk[-1]
         
-
     def getMin(self) -> int:
+        return self.currMinList[-1][1]
         
-
-
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
