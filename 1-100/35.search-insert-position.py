1fcb71428ddde70d5c0fa2a1a5c0b0bd31c2857c
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-02-20 11:02:20 +0800

    more elegant solution to 35. Search Insert Position

--- 1-100/35.search-insert-position.py
+++ 1-100/35.search-insert-position.py
@@ -1,15 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)-1
-        mid = (left+right)//2
 
         while left <= right:
+            mid = (left+right)//2
             if nums[mid] < target:
-                left += 1
+                left = mid + 1
             elif nums[mid] > target:
-                right -= 1
+                right = mid - 1
             else:
                 return mid
-            mid = (left+right)//2
-
+                
         return left
